ccab6bf1c691a3e40cdec956328be2505d050daf
Sandra <nannan.tian@shihengtech.com>
2022-08-01 14:58:09 +0800

    feat: 资源管理：无用资源隐藏

--- webpack.config.js
+++ webpack.config.js
@@ -6,44 +6,44 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/i,
-        // use: [
-        //   'file-loader',
-        // ]
-        type: 'asset/resource',
-      },
-      {
-        test: /\.txt$/,
-        use: 'raw-loader'
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader'
-        ]
-      },
-      {
-        test: /\.(csv|tsv)$/,
-        use: [
-          'csv-loader'
-        ]
-      },
-      {
-        test: /\.xml$/,
-        use: [
-          'xml-loader'
-        ]
-      }
-    ]
-  },
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.css$/i,
+  //       use: [
+  //         'style-loader',
+  //         'css-loader',
+  //       ]
+  //     },
+  //     {
+  //       test: /\.(png|svg|jpg|gif)$/i,
+  //       // use: [
+  //       //   'file-loader',
+  //       // ]
+  //       type: 'asset/resource',
+  //     },
+  //     {
+  //       test: /\.txt$/,
+  //       use: 'raw-loader'
+  //     },
+  //     {
+  //       test: /\.(woff|woff2|eot|ttf|otf)$/,
+  //       use: [
+  //         'file-loader'
+  //       ]
+  //     },
+  //     {
+  //       test: /\.(csv|tsv)$/,
+  //       use: [
+  //         'csv-loader'
+  //       ]
+  //     },
+  //     {
+  //       test: /\.xml$/,
+  //       use: [
+  //         'xml-loader'
+  //       ]
+  //     }
+  //   ]
+  // },
 };
